1638f88bdc6708a9cee01dbf93eeff0f22e04efe
WesselCode <70235259+WesselCode@users.noreply.github.com>
2021-09-28 16:22:02 -0700

    Added Scene Changing

---
+++ Between Light and Shadow/Assets/Scenes/SceneChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    //to change scene to first area on starting the game
+    public void startGame()
+    {
+        SceneManager.LoadScene("FirstArea");
+    }
+
+    public void exitToMainMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+}
