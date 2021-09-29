f70864457396e5dbdaf5c1cc77beb62b62079f2e
WesselCode <70235259+WesselCode@users.noreply.github.com>
2021-09-28 22:14:29 -0700

    Added Sprites and Scene Changes

--- Between Light and Shadow/Assets/Scenes/SceneChange.cs
+++ Between Light and Shadow/Assets/Scenes/SceneChange.cs
@@ -8,9 +8,37 @@ public class SceneChange : MonoBehaviour
     //to change scene to first area on starting the game
     public void startGame()
     {
+        //the center road area where you started
         SceneManager.LoadScene("FirstArea");
     }
-
+    public void toCellar()
+    {
+        SceneManager.LoadScene("Cellar");
+    }
+    public void toCenterHouse()
+    {
+        SceneManager.LoadScene("CenterHouse");
+    }
+    public void toLeftHouse()
+    {
+        SceneManager.LoadScene("LeftHouse");
+    }
+    public void toRightHouse()
+    {
+        SceneManager.LoadScene("RightHouse");
+    }
+    public void toRightShed()
+    {
+        SceneManager.LoadScene("RightShed");
+    }
+    public void toRightRoad()
+    {
+        SceneManager.LoadScene("RightRoad");
+    }
+    public void toLeftRoad()
+    {
+        SceneManager.LoadScene("LeftRoad");
+    }
     public void exitToMainMenu()
     {
         SceneManager.LoadScene("StartMenu");
