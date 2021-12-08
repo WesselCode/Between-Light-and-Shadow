be1989e4a97052b3c82037984eac8c82b4516cba
WesselCode <70235259+WesselCode@users.noreply.github.com>
2021-12-07 19:47:25 -0800

    checking build, everything working right now

---
+++ Between Light and Shadow/Assets/Scenes/Scripts/ButtonMonitor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ButtonMonitor : MonoBehaviour
+{
+    //in awake check button status:
+    //DOES NOTHING AS OF NOW
+    /*void Awake()//part of attempt to get buttons to stay off when entering scene
+     {
+         try
+         {
+             GameObject.Find("Water").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("Water");
+             GameObject.Find("EmptyBottle").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("EmptyBottle");
+             GameObject.Find("Can").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("Can");
+             GameObject.Find("Can2").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("Can2");
+             GameObject.Find("TableCloth").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("TableCloth");
+             GameObject.Find("CoatRack").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("CoatRack");
+             GameObject.Find("ClothingLine").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("ClothingLine");
+             GameObject.Find("Map").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("Map");
+             GameObject.Find("Product").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("Product");
+             GameObject.Find("BoltCutters").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("BoltCutters");
+             GameObject.Find("ShedKey").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("ShedKey");
+         }
+         catch (Exception e)
+         {
+            Debug.Log("button not in scene");
+            Debug.Log(e);
+         }
+     }*/
+}
--- Between Light and Shadow/Assets/Scenes/Scripts/addItems.cs
+++ Between Light and Shadow/Assets/Scenes/Scripts/addItems.cs
@@ -6,32 +6,6 @@ using System;
 
 public class addItems : MonoBehaviour
 {
-    //in awake check button status:
-    //DOES NOTHING AS OF NOW
-   /* void Awake()//part of attempt to get buttons to stay off when entering scene
-    {
-        try
-        {
-            GameObject.Find("Water").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("Water");
-            GameObject.Find("EmptyBottle").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("EmptyBottle");
-            GameObject.Find("Can").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("Can");
-            GameObject.Find("Can2").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("Can2");
-            GameObject.Find("TableCloth").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("TableCloth");
-            GameObject.Find("CoatRack").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("CoatRack");
-            GameObject.Find("ClothingLine").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("ClothingLine");
-            GameObject.Find("Map").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("Map");
-            GameObject.Find("Product").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("Product");
-            GameObject.Find("BoltCutters").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("BoltCutters");
-            GameObject.Find("ShedKey").GetComponent<UnityEngine.UI.Button>().interactable = GlobalControl.Instance.checkActive("ShedKey");
-        }
-        catch (Exception e)
-        {
-            Debug.Log("button not in scene");
-
-        }
-    }*/
-
-
     //to add items to the inventory instance, for use by buttons, a function a button
     //these are all on press for buttons in the first level, some must deactivate the buttons
     //use GetComponent<Button>().interactable = false; to disable buttons
