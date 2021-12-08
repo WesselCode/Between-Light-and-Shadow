97c4a5223296e5ad71359df434f3c21a18f1c269
WesselCode <70235259+WesselCode@users.noreply.github.com>
2021-12-07 20:16:11 -0800

    speech enabled

---
+++ Between Light and Shadow/Assets/Scenes/Scripts/SpeechBubbles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubbles : MonoBehaviour
+{
+    //on press will need to change make the button non-interactable
+    //and change color of text to disappear
+    public void getRidOfSpeech()
+    {
+        GameObject.Find("Speech").GetComponent<UnityEngine.UI.Button>().interactable = false;
+        //lower text opacity to 0
+        GameObject.Find("said").GetComponent<UnityEngine.UI.Text>().color = new Color(0, 0, 0, 0);
+    }
+
+    //this is the function that makes the button interactble and text black
+    //this occurs in one of two places right now
+    public void activateButton()
+    {
+        //first the Speech button
+        GameObject.Find("Speech").GetComponent<UnityEngine.UI.Button>().interactable = true;
+        //now change text to black, text is labeled "said":
+        GameObject.Find("said").GetComponent<UnityEngine.UI.Text>().color = new Color(0, 0, 0, 255);
+    }
+}
